3ad0b53db9d99ed0c9ba6198d85b5dc9e11858ae
水戸拓哉 <>
2021-10-16 20:24:17 +0900

    Add:ジェネレーターの設定変更

--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,11 @@ module Pomosto
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.skip_routes true
+      g.assets false
+      g.helper false
+      g.test_framework
+    end
   end
 end

f9686c9bf8cd8bf874062f6ebdd85b9ab2d0817e
水戸拓哉 <>
2021-10-25 14:39:45 +0900

    Add:ストレッチ画面にプログレスバー追加 #19

--- app/javascript/packs/worktimes/youtube.js
+++ app/javascript/packs/worktimes/youtube.js
@@ -9,4 +9,59 @@ window.onload = function() {
   urls[3] = 'https://www.youtube.com/embed/f2_PLjc6iqs?start=20&end=313';
   urls[4] = 'https://www.youtube.com/embed/3cN0XATgV8U?end=257';
   ifm.src = urls[r];
-}
+};
+
+(function($) {
+  $.fn.timer = function(totalTime) {
+    // reset timer
+    clearTimeout(this.data('id_of_settimeout'));
+    this.empty();
+    
+    // initialize elements
+    this.append('<div class="progress" style="height: 20px;"></div>');
+    this.children('.progress').append('<div class="progress-bar bg-success"></div>');
+    this.find('.progress-bar').css({
+        cssText: '-webkit-transition: none !important; transition: none !important;',
+        width: '100%'
+    });
+
+    var countdown = (function(timeLeft) {
+      var $progressBar = this.find('div.progress-bar');
+      var $header = this.children('h2');
+
+      if (timeLeft <= 0) {
+        $header.empty().text('休憩時間が終了しました。').addClass('text-danger');
+        return;
+      }
+
+      $header.children('span').text(timeLeft);
+
+      var width = (timeLeft - 1) * (100/totalTime); // unit in '%'
+      if (width < 10) { // less than 10 %
+        $progressBar.removeClass();
+        $progressBar.addClass('progress-bar bg-danger');
+      } else if (width < 20) { // less than 20 % (and more than 10 %)
+        $progressBar.removeClass();
+        $progressBar.addClass('progress-bar bg-warning');
+      }
+
+      $progressBar.animate({
+        width:  width + '%'
+      }, 1000, 'linear');
+
+      var id = setTimeout((function() {
+        countdown(timeLeft - 1);
+      }), 1000);
+      this.data("id_of_settimeout", id);
+    }).bind(this);
+
+    countdown(totalTime);
+  };
+})(jQuery);
+
+$(window).on('load', function (e){
+  e.preventDefault();
+
+  var duration = $("#duration").val();
+  $('#progress_bar').timer(300);
+});

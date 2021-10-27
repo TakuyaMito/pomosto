59d62c0b85eb5c57ecf936a90d9fd9c6939d13fc
水戸拓哉 <>
2021-10-27 09:35:23 +0900

    Add:ユーザーログイン機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_action :require_login, only: %i[new create]
 
   def new
     @user = User.new
@@ -16,6 +17,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :user)
+    params.require(:user).permit(:email, :password, :password_confirmation, :name)
   end
 end

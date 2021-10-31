5ac9f549bdd8f8569aaec72f18b03e024f682d9f
水戸拓哉 <>
2021-10-31 18:09:09 +0900

    Add:List作成画面の作成

---
+++ app/controllers/list_controller.rb
@@ -0,0 +1,20 @@
+class ListController < ApplicationController
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = current_user.list.build(list_params)
+    if @list.save
+      redirect_to :root
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:title)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'worktimes#index'
   get 'timer', to: 'worktimes#new'
 
+  resources :list, only: %i[new create]
+
   resources :users, only: %i[new create]
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'

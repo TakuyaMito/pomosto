e92def12c10b4dde1fe8f748710d1ab58053ad77
TakuyaMito <>
2021-11-19 12:10:14 +0900

    Add:Listモデルのテスト作成

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe List, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "フィールドが有効なとき" do
+    it "有効であること" do
+      list = build(:list)
+      expect(list).to be_valid
+    end
+  end
+
+  context "タイトルが存在しないとき" do
+    it "無効であること" do
+      list = build(:list, title: nil)
+      expect(list).to be_invalid
+      expect(list.errors[:title]).to include('を入力してください')
+    end
+  end
+
+  context "タイトルが255文字以下のとき" do
+    it "有効であること" do
+      list = build(:list, title: "a" * 255)
+      expect(list).to be_valid
+    end
+  end
+
+  context "タイトルが256文字以上のとき" do
+    it "無効であること" do
+      list = build(:list, title: "a" * 256)
+      expect(list).to be_invalid
+      expect(list.errors[:title]).to include("は255文字以内で入力してください")
+    end
+  end
 end

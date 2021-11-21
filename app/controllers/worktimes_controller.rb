2345cae026d63df284db01e61933ddb101adbb50
TakuyaMito <>
2021-11-21 18:38:16 +0900

    Add:ポモドーロ数を保存

--- app/controllers/worktimes_controller.rb
+++ app/controllers/worktimes_controller.rb
@@ -5,19 +5,13 @@ class WorktimesController < ApplicationController
   end
 
   def create
-    binding.pry
     @worktime = current_user.worktimes.build(worktime_params)
     @worktime.save
-      # respond_to do |format|
-      #   format.html { redirect_to root_path }
-      #   format.json { render json: { pomo_time: @worktime.pomo_time, user_id: @worktime.user_id, id: @worktime.id } }
-      # end
-    end
   end
 
   private
 
   def worktime_params
-    params.permit(:pomo_time)
+    params.require(:worktime).permit(:pomo_time)
   end
 end

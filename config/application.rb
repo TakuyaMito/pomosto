a5abdd89346c1f568f22a606a951aca55c6506ec
TakuyaMito <>
2021-11-22 19:41:01 +0900

    Fix:タイムゾーン変更

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,7 @@ module Pomosto
     config.i18n.default_locale = :ja
     # 言語ファイルのパス
     config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end

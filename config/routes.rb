d413237e872a6c55d83a9f1dc5589bc8f1a58654
TakuyaMito <>
2021-11-23 22:05:05 +0900

    Fix:root_path修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root 'tops#index'
+  root 'homes#index'
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
-  get 'home', to: 'homes#index'
+  get 'top', to: 'tops#index'
 
   resources :lists, only: %i[new create edit update destroy] do
     resources :cards, except: %i[index]

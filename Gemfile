c6f05e85b8503b18ebda66d61c71107e2804f374
TakuyaMito <>
2021-11-24 12:11:06 +0900

    Fix:本番環境でメールが送信されるように修正送信されるように修正

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'chartkick'
 gem 'groupdate'
 gem 'bootsnap', '>= 1.4.4', require: false
 gem 'config', '2.2.3'
+gem 'dotenv-rails'
 
 group :development, :test do
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "pomosto_production"
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -117,4 +117,16 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_mailer.perform_caching = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            ENV['LOGIN_NAME'],
+    password:             ENV['LOGIN_PASSWORD'],
+    authentication:       'login',
+    enable_starttls_auto: true
+  }  
 end
